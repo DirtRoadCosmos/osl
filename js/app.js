02aa4e48836711447425991a1ebab60584ea4a3e
andy <andytilia@gmail.com>
2023-06-24 12:55:25 -0400

    initial

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// app.js
+// import Game from "./models/Game.js";
+// import GameList from "./models/GameList.js";
+// import View from "./views/View.js";
+// import Controller from "./controllers/Controller.js";
+
+// const gameList = new GameList();
+// const view = new View();
+// const controller = new Controller(gameList, view);
